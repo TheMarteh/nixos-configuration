624641d9b17955ddaec2d59bade2727bc1b8be0e
TheMarteh <martijnfs@me.com>
2026-01-23 02:01:57 +0100

    auto copy to system clipboard

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -9,3 +9,6 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
 vim.opt.expandtab = true
+
+-- auto copy to system clipboard
+vim.api.nvim_set_option("clipboard", "unnamedplus")
