442da96e6f04a71df0f8ba59ac9359a6372f075c
TheMarteh <martijnfs@me.com>
2026-02-16 19:14:36 +0100

    move to lazyvim

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,14 +1,3 @@
--- config options
-
--- line numbers
-vim.opt.relativenumber = true
-vim.opt.number = true
-
--- indentation and tabs
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.autoindent = true
-vim.opt.expandtab = true
-
--- auto copy to system clipboard
-vim.api.nvim_set_option("clipboard", "unnamedplus")
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
