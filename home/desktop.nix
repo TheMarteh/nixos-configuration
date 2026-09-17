df2d0092d88b973b8703476c4c2dd2857823ed7e
TheMarteh <martijnfs@me.com>
2026-09-17 08:21:40 +0200

    add hyprshutdown

--- home/desktop.nix
+++ home/desktop.nix
@@ -6,6 +6,9 @@
     # Wallpaper daemon (opvolger van swww), gestart via exec-once in hyprland.conf.
     # Niet via services.swww: die module start hardcoded `swww-daemon`.
     pkgs-unstable.awww
+    # Sluit eerst alle apps netjes af (zodat o.a. Firefox zijn sessie opslaat)
+    # en stopt daarna pas Hyprland. Staat nog niet in 25.11.
+    pkgs-unstable.hyprshutdown
     rofi
     waybar
     brightnessctl
--- home/shell.nix
+++ home/shell.nix
@@ -8,6 +8,9 @@
       nrs = "nh os switch"; # rebuild + switch, toont eerst een diff (vraagt zelf om sudo)
       nfu = "nix flake update --flake ~/nixos-configuration";
       nrsu = "nh os switch --update"; # nfu + nrs in één keer
+      # Via hyprshutdown, zodat apps eerst netjes afsluiten (`sudo reboot` omzeilt dit)
+      reboot = "hyprshutdown -t 'Herstarten...' -p 'systemctl reboot'";
+      poweroff = "hyprshutdown -t 'Afsluiten...' -p 'systemctl poweroff'";
     };
   };
 }
