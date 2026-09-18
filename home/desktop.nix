bd7cb0c00c84766dcf85e48d9140fa51943f738d
TheMarteh <martijnfs@me.com>
2026-09-18 12:36:50 +0200

    proberen mega upgrade te fiksen

--- home/desktop.nix
+++ home/desktop.nix
@@ -1,6 +1,11 @@
 { pkgs, pkgs-unstable, ... }:
 
 {
+  # Polkit-authenticatieagent: zonder deze faalt elke rechtenprompt stil
+  # (blueman, 1Password systeemauthenticatie, virt-manager, ...).
+  # Hangt zelf aan graphical-session.target, dus geen exec-once nodig.
+  services.hyprpolkitagent.enable = true;
+
   # Wayland/Hyprland tools
   home.packages = with pkgs; [
     # Wallpaper daemon (opvolger van swww), gestart via exec-once in hyprland.conf.
@@ -9,6 +14,8 @@
     # Sluit eerst alle apps netjes af (zodat o.a. Firefox zijn sessie opslaat)
     # en stopt daarna pas Hyprland. Staat nog niet in 25.11.
     pkgs-unstable.hyprshutdown
+    # nm-applet (tray) + nm-connection-editor voor de waybar-klik
+    networkmanagerapplet
     rofi
     waybar
     brightnessctl
--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -7,6 +7,7 @@
       "wheel" # sudo
       "docker" # docker zonder sudo
       "libvirtd" # virt-manager zonder wachtwoordprompt
+      "networkmanager" # netwerken beheren zonder polkit-prompt
     ];
     # Gebruikerspakketten staan in ./home
   };
